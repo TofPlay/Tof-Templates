ee8be43b45076fdba76f9a256c7c042af2549633
Christophe Braud <chbperso@gmail.com>
2015-11-24 16:26:39 +0100

    - Define public and private sections for extension

--- File Templates/Tof Templates/Swift types.xctemplate/ExtensionNSObject/___VARIABLE_extendedClass:identifier___+___VARIABLE_productName:identifier___.swift
+++ File Templates/Tof Templates/Swift types.xctemplate/ExtensionNSObject/___VARIABLE_extendedClass:identifier___+___VARIABLE_productName:identifier___.swift
@@ -10,12 +10,52 @@
 import Foundation
 
 // MARK: -
-// MARK: ___FILEBASENAMEASIDENTIFIER___ Extends ___VARIABLE_extendedClass:identifier___
+// MARK: ___VARIABLE_extendedClass:identifier___ extension
 // MARK: -
-extension ___VARIABLE_extendedClass:identifier___ {
-  // MARK: -> Class methods
+public  extension ___VARIABLE_extendedClass:identifier___ {
+  // MARK: -
+  // MARK: Public access
+  // MARK: -
   
-  // MARK: -> Init methods
+  // MARK: -> Public enums
+  
+  // MARK: -> Public structs
+  
+  // MARK: -> Public class
+  
+  // MARK: -> Public type alias 
+  
+  // MARK: -> Public static properties
+  
+  // MARK: -> Public properties
+  
+  // MARK: -> Public class methods
+  
+  // MARK: -> Public init methods
   
   // MARK: -> Public methods
+  
+  // MARK: -> Public protocol <#protocol name#>
+  
+  // MARK: -
+  // MARK: Private access
+  // MARK: -
+  
+  // MARK: -> Private enums
+  
+  // MARK: -> Private structs
+  
+  // MARK: -> Private class
+  
+  // MARK: -> Private type alias 
+
+  // MARK: -> Private static properties
+
+  // MARK: -> Private properties
+  
+  // MARK: -> Private class methods
+  
+  // MARK: -> Private init methods
+  
+  // MARK: -> Private methods
 }
