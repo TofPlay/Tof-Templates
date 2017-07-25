1ac9d881acf6fa35c14068f51b759c06240b654e
TofPlay <chbperso@gmail.com>
2017-07-25 09:18:45 +0200

    - Add 'Codable' template for iOS Apps

---
+++ File Templates/Tof Templates/iOS Application Class.xctemplate/Codable/___FILEBASENAME___.swift
@@ -0,0 +1,126 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Base on Tof Templates (https://goo.gl/GdyFiw)
+//  ___COPYRIGHT___
+//
+
+import Foundation
+
+// MARK: -
+// MARK: ___FILEBASENAMEASIDENTIFIER___
+// MARK: -
+internal class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+  // MARK: -
+  // MARK: Internal access (aka public for current module)
+  // MARK: -
+  
+  // MARK: -> Internal enums
+  
+  //public enum CodingKeys: String, CodingKey {
+  //  case <#field#>
+  //}
+
+  // MARK: -> Internal structs
+  
+  // MARK: -> Internal class
+  
+  // MARK: -> Internal type alias 
+  
+  // MARK: -> Internal static properties
+  
+  // MARK: -> Internal properties
+  
+  //internal var <#field#>:Bool? = nil
+  //internal var <#field#>:Int? = nil
+  //internal var <#field#>:Float? = nil
+  //internal var <#field#>:Double? = nil
+  //internal var <#field#>:Data? = nil
+  //internal var <#field#>:Date? = nil
+  //internal var <#field#>:Data? = nil
+  //internal var <#field#>:String? = nil
+
+  // MARK: -> Internal class methods
+  
+  // MARK: -> Internal init methods
+  
+  public init() {
+  }
+    
+  // MARK: -> Internal methods
+    
+  // MARK: -> Public protocol Encodable
+    
+  //public func encode(to pEncoder: Encoder) throws {
+  //  var lContainer = pEncoder.container(keyedBy: CodingKeys.self)
+  //
+  //  try lContainer.encode(self.<#field#>, forKey: .<#field#>)
+  //}
+  
+  // MARK: -> Public protocol Decodable
+    
+  //public required init(from pDecoder: Decoder) throws {
+  //  if let lValues = try? pDecoder.container(keyedBy: CodingKeys.self) {
+  //    self.<#field#> = try? lValues.decode(Bool.self, forKey: .<#field#>)
+  //    self.<#field#> = try? lValues.decode(Int.self, forKey: .<#field#>)
+  //    self.<#field#> = try? lValues.decode(Float.self, forKey: .<#field#>)
+  //    self.<#field#> = try? lValues.decode(Double.self, forKey: .<#field#>)
+  //    self.<#field#> = try? lValues.decode(Date.self, forKey: .<#field#>)
+  //    self.<#field#> = try? lValues.decode(Data.self, forKey: .<#field#>)
+  //    self.<#field#> = try? lValues.decode(String.self, forKey: .<#field#>)
+  //  }
+  //}
+
+  // MARK: -> Internal implementation protocol <#protocol name#>
+  
+  // MARK: -
+  // MARK: File Private access
+  // MARK: -
+  
+  // MARK: -> File Private enums
+  
+  // MARK: -> File Private structs
+  
+  // MARK: -> File Private class
+  
+  // MARK: -> File Private type alias 
+
+  // MARK: -> File Private static properties
+
+  // MARK: -> File Private properties
+  
+  // MARK: -> File Private class methods
+  
+  // MARK: -> File Private init methods
+  
+  // MARK: -> File Private operators
+
+  // MARK: -> File Private methods
+
+  // MARK: -
+  // MARK: Private access
+  // MARK: -
+  
+  // MARK: -> Private enums
+  
+  // MARK: -> Private structs
+  
+  // MARK: -> Private class
+  
+  // MARK: -> Private type alias 
+
+  // MARK: -> Private static properties
+
+  // MARK: -> Private properties
+
+  // MARK: -> Private class methods
+  
+  // MARK: -> Private init methods
+  
+  // MARK: -> Private operators
+
+  // MARK: -> Private methods
+    
+}
