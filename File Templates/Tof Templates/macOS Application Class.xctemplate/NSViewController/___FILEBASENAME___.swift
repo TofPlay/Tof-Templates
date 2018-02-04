b9d32bd45855f59224f2149bb957116d5072ae9b
TofPlay <chbperso@gmail.com>
2018-02-04 11:42:33 +0100

    Improves macOS applications templates to follow the best practice with Swift 4

--- File Templates/Tof Templates/macOS Application Class.xctemplate/NSViewController/___FILEBASENAME___.swift
+++ File Templates/Tof Templates/macOS Application Class.xctemplate/NSViewController/___FILEBASENAME___.swift
@@ -12,18 +12,18 @@ import Cocoa
 // MARK: -
 // MARK: ___FILEBASENAMEASIDENTIFIER___
 // MARK: -
-internal class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+public class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
   // MARK: -
   // MARK: Interface
   // MARK: -
   
-  // MARK: -> Interface Builder properties
+  // MARK: Interface Builder properties
   
-  // MARK: -> Interface Builder actions
+  // MARK: Interface Builder actions
   
-  // MARK: -> Interface navigation
+  // MARK: Interface navigation
   
-  //override internal func prepare(for pSegue: NSStoryboardSegue, sender pSender: Any?) {
+  //public public override func prepare(for pSegue: NSStoryboardSegue, sender pSender: Any?) {
   //  if pSegue.identifier == "<#segue name#>" {
   //    let l<#View Controller#> = pSegue.destination as! <#View Controller#>
   //  } else if pSegue.identifier == "<#segue name#>" {
@@ -31,51 +31,81 @@ internal class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___
   //  }
   //}
   
-  // MARK: -> Interface notifications
+  // MARK: Interface notifications
   
-  //internal func notification<#notification name#>(_ pNotification: NSNotification) {
+  //public func notification<#notification name#>(_ pNotification: NSNotification) {
   //  if pNotification.object is <#type#> {
   //    // <#notification code#>
   //  }
   //}
   
-  // MARK: -> Interface class override NSViewController
+  // MARK: Interface override properties
   
-  override internal func viewDidLoad() {
+  public override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+  // MARK: Interface class override NSViewController
+  
+  public override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
   
-  //override func viewWillAppear() {
+  //public override func viewWillAppear() {
   //  // Your code here
   //}
   
-  //override func viewDidAppear() {
+  //public override func viewDidAppear() {
   //  // Your code here
   //}
   
-  //override func updateViewConstraints() {
+  //public override func updateViewConstraints() {
   //  // Your code here
   //}
   
-  //override func viewWillLayout() {
+  //public override func viewWillLayout() {
   //  // Your code here
   //}
   
-  //override func viewDidLayout() {
+  //public override func viewDidLayout() {
   //  // Your code here
   //}
   
-  //override func viewWillDisappear() {
+  //public override func viewWillDisappear() {
   //  // Your code here
   //}
   
-  //override func viewDidDisappear() {
+  //public override func viewDidDisappear() {
   //  // Your code here
   //}
   
-  // MARK: -> Interface implementation protocol <#protocol name#>
+  // MARK: -
+  // MARK: Public access
+  // MARK: -
+  
+  // MARK: -> Public enums
+  
+  // MARK: -> Public structs
+  
+  // MARK: -> Public class
+  
+  // MARK: -> Public type alias 
+  
+  // MARK: -> Public static properties
+  
+  // MARK: -> Public properties
+  
+  // MARK: -> Public class methods
+  
+  // MARK: -> Public init methods
+  
+  // MARK: -> Public operators
 
+  // MARK: -> Public methods
+    
   // MARK: -
   // MARK: Internal access (aka public for current module)
   // MARK: -
@@ -90,49 +120,11 @@ internal class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___
   
   // MARK: -> Internal static properties
   
-  // MARK: -> Internal properties
-  
   // MARK: -> Internal class methods
-  
-  // MARK: -> Internal init methods
   
   // MARK: -> Internal operators
 
   // MARK: -> Internal methods
-  
-  // MARK: -> Internal class override ___VARIABLE_cocoaTouchSubclass___
-  
-  override internal var representedObject: Any? {
-    didSet {
-    // Update the view, if already loaded.
-    }
-  }
-
-  // MARK: -> Internal implementation protocol <#protocol name#>
-  
-  // MARK: -
-  // MARK: File Private access
-  // MARK: -
-  
-  // MARK: -> File Private enums
-  
-  // MARK: -> File Private structs
-  
-  // MARK: -> File Private class
-  
-  // MARK: -> File Private type alias 
-
-  // MARK: -> File Private static properties
-
-  // MARK: -> File Private properties
-  
-  // MARK: -> File Private class methods
-  
-  // MARK: -> File Private init methods
-  
-  // MARK: -> File Private operators
-
-  // MARK: -> File Private methods
 
   // MARK: -
   // MARK: Private access
@@ -157,4 +149,70 @@ internal class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___
   // MARK: -> Private operators
 
   // MARK: -> Private methods
+
 }
+
+// MARK: -
+// MARK: -> Custom implementation protocol <#custom protocol#>
+// MARK: -
+//extension ___FILEBASENAMEASIDENTIFIER___:  <#custom protocol#> {
+  // MARK: -
+  // MARK: Public access
+  // MARK: -
+  
+  // MARK: -> Public enums
+  
+  // MARK: -> Public structs
+  
+  // MARK: -> Public class
+  
+  // MARK: -> Public type alias 
+  
+  // MARK: -> Public static properties
+  
+  // MARK: -> Public class methods
+  
+  // MARK: -> Public operators
+
+  // MARK: -> Public methods
+  
+  // MARK: -
+  // MARK: Public access
+  // MARK: -
+  
+  // MARK: -> Public enums
+  
+  // MARK: -> Public structs
+  
+  // MARK: -> Public class
+  
+  // MARK: -> Public type alias 
+  
+  // MARK: -> Public static properties
+  
+  // MARK: -> Public class methods
+  
+  // MARK: -> Public operators
+
+  // MARK: -> Public methods
+  
+  // MARK: -
+  // MARK: Private access
+  // MARK: -
+  
+  // MARK: -> Private enums
+  
+  // MARK: -> Private structs
+  
+  // MARK: -> Private class
+  
+  // MARK: -> Private type alias 
+
+  // MARK: -> Private static properties
+
+  // MARK: -> Private class methods
+  
+  // MARK: -> Private operators
+
+  // MARK: -> Private methods
+//}  
